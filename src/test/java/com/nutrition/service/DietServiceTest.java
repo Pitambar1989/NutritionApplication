fac7b97a040ed867986c0ba2ee08025c735a0f4f
Pitambar <pk.java89@gmail.com>
2021-10-08 17:16:44 +0530

    DietService test class commented

--- src/test/java/com/nutrition/service/DietServiceTest.java
+++ src/test/java/com/nutrition/service/DietServiceTest.java
@@ -6,29 +6,6 @@ import org.junit.Test;
 
 public class DietServiceTest {
 
-	@Test
-	public void testGetDietPlan() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSaveDietPlan() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSaveFoodDetails() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetDietDetails() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetFoodDetails() {
-		fail("Not yet implemented");
-	}
+	
 
 }
